60c33aa9a209bbfc5aaa02ecf404800f00ae0714
u0d7i <u0d7i@users.noreply.github.com>
2016-01-17 19:59:04 +0200

    connectivity

---
+++ connectivity.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+CFG="connectivity.gconf.cpt"
+# prepate it with
+# gconftool -R /system/osso/connectivity/IAP/<conn_id>
+# encrypt with ccrypt
+# save to modified rootfs together with this scrupt
+
+if [ -z "$(command -v ccrypt)" ]; then
+        echo "ccrypt not installed"
+        exit
+fi
+
+if [ ! -f ${CFG} ]; then
+        echo "configuration file not found"
+        exit
+fi
+
+# clean
+gconftool -R /system/osso/connectivity/IAP | grep "^ /" | sed 's/:$//' | while read line
+do
+        gconftool --dump ${line} | gconftool --unload -
+done
+
+# load
+ccrypt -c ${CFG} | gconftool --load -
