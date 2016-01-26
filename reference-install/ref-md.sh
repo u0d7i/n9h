0f5f1df29d36229be6cdad06dd8466ca687f952e
u0d7i <u0d7i@users.noreply.github.com>
2016-01-26 13:25:22 +0200

    purge

--- reference-install/ref-md.sh
+++ reference-install/ref-md.sh
@@ -12,6 +12,7 @@ usage(){
     echo "  apt  - set apt sources file"
     echo "  conn - set up connectivity"
     echo "  cssu - install CSSU"
+    echo "  purge - remove crap"
     echo
     exit
 }
@@ -43,26 +44,27 @@ conn(){
     fi
     # add status check and cli enable later
     # start gui and use dirty loos for now
-    dbus-send --system --type=method_call \
-        --dest=com.nokia.icd_ui /com/nokia/icd_ui \
-        com.nokia.icd_ui.show_conn_dlg boolean:false
+    #dbus-send --system --type=method_call \
+    #    --dest=com.nokia.icd_ui /com/nokia/icd_ui \
+    #    com.nokia.icd_ui.show_conn_dlg boolean:false
 }
 
 apttest(){
     # check for internet connection
     # there are several ways to do it
     # but the only reliable one - try using actual resource you need
+    #
     url=$(head -1 $apt_src | cut -d' ' -f2)
     if ! echo $url | grep 'repository.maemo.org' > /dev/null 2>&1; then
         echo "-err: unexpected apt source"
         return 1
     fi
     if wget -q -O - $url >/dev/null 2>&1; then
-        echo "+ok: apt source is reachable"
+        [ "$1" == "q" ] || echo "+ok: apt source is reachable"
         return 0
     else
-       echo "-err: apt source is not reachable"
-       return 1
+        [ "$1" == "q" ] || echo "-err: apt source is not reachable"
+        return 1
     fi
 }
 
@@ -78,6 +80,27 @@ cssu(){
     fi
 }
 
+purge(){
+    apt-get -y remove --purge cherry ap-installer amazon-installer \
+        foreca-installer facebook-installer dtg-installer \
+        tutorial-home-applet osso-tutorial-l10n-engb \
+        osso-tutorial-l10n-ptpt osso-tutorial-l10n-frca \
+        osso-tutorial-l10n-nlnl osso-tutorial-l10n-cscz \
+        osso-tutorial-l10n-itit osso-tutorial-l10n-eses \
+        osso-tutorial-l10n-svse osso-tutorial-l10n-frfr \
+        osso-tutorial-l10n-dede osso-tutorial-l10n-fifi \
+        osso-tutorial-l10n-nono osso-tutorial-l10n-esmx \
+        osso-tutorial-l10n-enus osso-tutorial-l10n-ruru \
+        osso-tutorial-l10n-mr0 osso-tutorial-l10n-plpl \
+        osso-systemui-splashscreen sharing-service-flickr \
+        sharing-service-ovi chinese-font google-search-widget \
+        tutorial-home-applet ovi-promotion-widget gnuchess \
+        hildon-theme-beta maemoblocks osso-chess-ui \
+        osso-graphics-game-chess osso-graphics-game-lmarbles \
+        osso-graphics-game-mahjong osso-lmarbles osso-mahjong \
+        osso-sounds-game-chess osso-sounds-game-mahjong \
+        hildon-welcome hildon-welcome-default-logo
+}
 
 # fascist checks
 grep "RX-51" /proc/cpuinfo > /dev/null && \
@@ -96,10 +119,14 @@ case $1 in
         echo "not implemented yet"
         aptfile
         conn
-        while ! cssu
+        while ! apttest q
         do
+            echo -n .
             sleep 1
         done
+        echo
+        cssu
+        purge
         ;;
     apt)
         aptfile
@@ -109,6 +136,9 @@ case $1 in
         ;;
     cssu)
         cssu
+        ;;
+    purge)
+        purge
         ;;
     *)
         usage
